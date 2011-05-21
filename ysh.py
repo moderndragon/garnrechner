4335bc1068db0112d00c753dd301e354c38cd729
ryuu <thursa@riesenheim.net>
2011-05-21 17:32:24 +0200

    yarn substitution helper now with yarn names, too

--- ysh.py
+++ ysh.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 # This Python file uses the following encoding: utf-8
 import os, sys 
+# name of yarn a
+na = raw_input("What is the name of the yarn given in the pattern?")
 
 # meters per skein of yarn a
-la = int(raw_input("please specify the length of the yarn you want to substitute:"))
+la = int(raw_input("How many meters is a skein of the yarn given in the pattern? "))
+
+#name of yarn b
+nb = raw_input("What is the name of your yarn?")
 
 # meters per skein of yarn b
-lb = int(raw_input("please specify the length of your yarn:"))
+lb = int(raw_input("How many meters is a skein of your yarn? "))
 
 # number of skeins of yarn a
-x = int(raw_input("please specify the number of skeins given in the pattern (integer):"))
+x = int(raw_input("please specify the number of skeins given in the pattern (integer): "))
 
 # total length
 ltot = la * x
@@ -18,4 +23,4 @@ ltot = la * x
 y = x * la // lb + 1
 
 print "You need", ltot, "meters of yarn for your project."
-print "You need", y, "skeins of your yarn."
+print "You need", y, "skeins of",  nb,"."
